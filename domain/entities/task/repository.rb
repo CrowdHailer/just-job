ab38f47fb1ff4b722051862f378c8445206dded9
CrowdHailer <peterhsaxton@gmail.com>
2015-08-14 14:25:40 +0100

    implement task repository

--- domain/entities/task/repository.rb
+++ domain/entities/task/repository.rb
@@ -1,7 +1,24 @@
 class Task
   module Repository
+    @contents = []
     def self.for_todo_list(name)
-      []
+      contents.select { |task| task.todo_list == name }
+    end
+
+    def self.contents
+      @contents
+    end
+
+    def self.add(task)
+      contents << task
+    end
+
+    def self.clear
+      contents.clear
+    end
+
+    def self.empty?
+      contents.empty?
     end
   end
 end
--- test/entities/task/task_repository_test.rb
+++ test/entities/task/task_repository_test.rb
@@ -5,7 +5,29 @@ require_relative '../../../domain/entities/task/repository'
 class Task
   class RespositoryTest < JustJobTest
     def test_returns_empty_list_when_no_tasks
-      assert_empty Task::Repository.for_todo_list "no list"
+      assert_empty Repository.for_todo_list "no list"
     end
+
+    def test_can_add_a_task
+      task = Task.new "my list", "take the high road"
+      Repository.add task
+      refute Repository.empty?
+    end
+
+    def test_can_clear_repository
+      task = Task.new "my list", "take the high road"
+      Repository.add task
+      Repository.clear
+      assert Repository.empty?
+    end
+
+    def test_returns_all_tasks_in_a_todo_list
+      my_task = Task.new "my list", "take the high road"
+      your_task = Task.new "your list", "take the low road"
+      Repository.add my_task
+      Repository.add your_task
+      assert_equal [my_task], Repository.for_todo_list("my list")
+    end
+
   end
 end
