3910841afdad7a85ea28d8b50a625012010ce9c2
CrowdHailer <peterhsaxton@gmail.com>
2015-08-14 20:17:21 +0100

    implement instruction value object

--- domain/values/instruction.rb
+++ domain/values/instruction.rb
@@ -1,5 +1,11 @@
 class Instruction
+  VALIDATOR = /^[^<>]{2,255}$/
   def initialize(raw)
-    
+    raise ArgumentError, "Invalid instruction" unless VALIDATOR.match(raw)
+    @raw = raw
+  end
+
+  def to_s
+    @raw
   end
 end
--- test/values/instruction_test.rb
+++ test/values/instruction_test.rb
@@ -7,4 +7,27 @@ class InstructionTest < JustJobTest
       Instruction.new "Open the pod bay doors"
     end
   end
+
+  def test_has_same_string_value_as_raw_value
+    instruction = Instruction.new "Open the pod bay doors"
+    assert_equal "Open the pod bay doors", instruction.to_s
+  end
+
+  def test_cannot_be_created_with_less_than_2_charachters
+    assert_raises ArgumentError do
+      Instruction.new "a"
+    end
+  end
+
+  def test_cannot_be_created_with_more_than_255_charachters
+    assert_raises ArgumentError do
+      Instruction.new "a" * 256
+    end
+  end
+
+  def test_cannot_contain_angled_brackets
+    assert_raises ArgumentError do
+      Instruction.new "<bad behaviour>"
+    end
+  end
 end
