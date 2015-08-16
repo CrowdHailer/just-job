852798db1f6040d470b64e3c98300263e395ea0e
CrowdHailer <peterhsaxton@gmail.com>
2015-08-16 12:07:28 +0100

    implement validations and equality on the todo list name value object

--- domain/values/todo_list_name.rb
+++ domain/values/todo_list_name.rb
@@ -1,5 +1,15 @@
 class TodoListName
+  VALIDATOR = /^[^<>]{2,80}$/
   def initialize(raw)
+    raise ArgumentError, "Invalid name" unless VALIDATOR.match(raw)
+    @raw = raw
+  end
 
+  def to_s
+    @raw
+  end
+
+  def ==(other)
+    other.is_a?(TodoListName) && to_s == other.to_s
   end
 end
